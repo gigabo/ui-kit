d6ab48b7ac575244cecfee55a6ac6a09f30f9272
Carey Spies <carey.spies@gmail.com>
2016-04-28 23:40:53 -0700

    added a button and lots of sass

--- www/index.js
+++ www/index.js
@@ -3,6 +3,16 @@ import './styles/index.css'
 
 export default class SimplePage {
 	getElements () {
-		return <h1>Hello world</h1>;
+		return (
+		<div>
+			<h1>Heading 1</h1>
+			<h2>Heading 2</h2>
+			<h3>Heading 3</h3>
+			<hr/>
+			<button>Click Me</button>
+			<button>Love Me</button>
+			<button>Press my Buttons</button>
+
+			</div>		);
 	}
 }
